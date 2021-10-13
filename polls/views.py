055901ae3ecf7325abec249e1cb765b499002451
Trevor Stahl <trevorclarkstahl@gmail.com>
2021-10-13 12:26:31 -0700

    done with initial templating

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from django.http import Http404
-# from django.template import loader
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 
@@ -11,20 +9,9 @@ def index(request):
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
-# def index(request):
-#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#     template = loader.get_template('polls/index.html')
-#     context = {
-#         'latest_question_list': latest_question_list,
-#     }
-#     return HttpResponse(template.render(context, request))
-
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 
